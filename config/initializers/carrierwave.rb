9d2189a6b36e9bfaf6745f9168cab8533ec77d0f
robo-hobo <robertzielinski@dobrapogoda24.pl>
2015-12-16 09:42:58 +0100

    amazon data

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,9 +6,7 @@ CarrierWave.configure do |config|
   # For testing, upload files to local `tmp` folder.
   if Rails.env.test? || Rails.env.development?
     config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
+  elsif Rails.env.production?
     config.storage = :fog
     config.fog_credentials = {
       # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
