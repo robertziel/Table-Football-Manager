beaf0c8427328847773c2ec8b4af066c79a4e98d
robo-hobo <robertzielinski@dobrapogoda24.pl>
2015-12-16 09:17:22 +0100

    amazon data

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,9 +13,9 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET'],
-      :region                => ENV['S3_REGION']
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                => 'eu-central-1'
     }
 
     config.fog_directory    = ENV['S3_BUCKET_NAME']
